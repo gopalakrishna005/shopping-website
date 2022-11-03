ead173f3d0fac6dfd5442efdf2e1ac3470699ae6
Gopala krishnan <113330207+gopalakrishna005@users.noreply.github.com>
2022-11-03 20:17:37 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "Casual Shirt",
+      price: 360,
+      desc: "Campus Sutra Men Blue & Grey Regular Fit Colourblocked Casual Shirt",
+      img: "images/imag3.webp",
+    },
+    {
+      id: "ioytrhndcv",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "thyfhcbcv",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+  ];
